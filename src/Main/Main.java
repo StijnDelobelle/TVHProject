0e965c1589deead9b826b060f3c2f0b01873018e
stijndelobelle <stijndelobelle@icloud.com>
2018-11-10 20:14:10 +0100

    Big structure change - Cleaned

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,6 +9,7 @@ public class Main {
      java -jar tvh-1.0-student.jar --problem=tvh_problem_3.txt --solution=tvh_solution_3.txt --gui
      */
 
+
     public static String INPUT_FILE = "tvh_problem_3.txt";
     public static String OUTPUT_FILE = "tvh_solution_3.txt";
 
